f32b3b8017bba69ce3b86729cad449877e7d61e4
ErikBatista <erikbatista42@gmail.com>
2018-09-13 23:18:14 -0700

    completed grid challenge

--- Projects/Squares-Grid-Challenge/Game-Starter-Empty/GameScene.swift
+++ Projects/Squares-Grid-Challenge/Game-Starter-Empty/GameScene.swift
@@ -23,10 +23,9 @@ class GameScene: SKScene {
         let action = SKAction.run {
             self.topRow()
             self.middleRow()
-
+            self.bottomRow()
         }
         self.run(action)
-        
     }
     
     func topRow() {
@@ -59,8 +58,6 @@ class GameScene: SKScene {
             addChild(square)
         }
         thirdSquare()
-        
-        
     }
     
     func middleRow() {
@@ -75,7 +72,7 @@ class GameScene: SKScene {
         }
         firstSquare()
         
-        func secondSquare() {
+    func secondSquare() {
             let size = CGSize(width: 50, height: 50)
             let square = SKSpriteNode(texture: nil, color: .magenta, size: size)
             if let view = self.view {
@@ -99,5 +96,36 @@ class GameScene: SKScene {
         thirdSquare()
     }
     
+    func bottomRow() {
+        func firstSquare() {
+            let size = CGSize(width: 50, height: 50)
+            let square = SKSpriteNode(texture: nil, color: .magenta, size: size)
+            // bounds is the size
+            square.position.x = 120
+            square.position.y = 270
+            addChild(square)
+        }
+        firstSquare()
+        
+        func secondSquare() {
+            let size = CGSize(width: 50, height: 50)
+            let square = SKSpriteNode(texture: nil, color: .magenta, size: size)
+            if let view = self.view {
+                square.position.x = view.bounds.width/2
+                square.position.y = 270
+            }
+            addChild(square)
+        }
+        secondSquare()
+        
+        func thirdSquare() {
+            let size = CGSize(width: 50, height: 50)
+            let square = SKSpriteNode(texture: nil, color: .magenta, size: size)
+            square.position.x = 250
+            square.position.y = 270
+            addChild(square)
+        }
+        thirdSquare()
+    }
     
 }
