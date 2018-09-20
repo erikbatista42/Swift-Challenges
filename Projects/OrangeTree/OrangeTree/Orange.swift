72852bf1191ac77f3cdf26fe95526b4542209164
ErikBatista <erikbatista42@gmail.com>
2018-09-20 15:06:44 -0700

    added orange class

---
+++ Projects/OrangeTree/OrangeTree/Orange.swift
@@ -0,0 +1,23 @@
+//
+//  Orange.swift
+//  OrangeTree
+//
+//  Created by Erik Batista on 9/20/18.
+//  Copyright © 2018 Erik Batista. All rights reserved.
+//
+import SpriteKit
+
+class Orange: SKSpriteNode {
+    
+    init() {
+        let texture = SKTexture(imageNamed: "Orange")
+        let size = texture.size()
+        let color = UIColor.clear
+        
+        super.init(texture: texture, color: color, size: size)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
