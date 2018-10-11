982e060a5052e49152258bf2e9321133f3b0dbfc
ErikBatista <erikbatista42@gmail.com>
2018-10-10 21:20:02 -0700

    code is working

--- Projects/SushiNeko/SushiNeko/SushiPiece.swift
+++ Projects/SushiNeko/SushiNeko/SushiPiece.swift
@@ -14,6 +14,24 @@ class SushiPiece: SKSpriteNode {
     var rightChopstick: SKSpriteNode!
     var leftChopstick: SKSpriteNode!
     
+    /* Sushi type */
+    var side: Side = .none {
+        didSet {
+            switch side {
+            case .left:
+                /* Show left chopstick */
+                leftChopstick.isHidden = false
+            case .right:
+                /* Show right chopstick */
+                rightChopstick.isHidden = false
+            case .none:
+                /* Hide all chopsticks */
+                leftChopstick.isHidden = true
+                rightChopstick.isHidden = true
+            }
+        }
+    }
+    
     /* You are required to implement this for your subclass to work */
     override init(texture: SKTexture?, color: UIColor, size: CGSize) {
         super.init(texture: texture, color: color, size: size)
@@ -22,6 +40,14 @@ class SushiPiece: SKSpriteNode {
     /* You are required to implement this for your subclass to work */
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
+    }
+    
+    func connectChopsticks() {
+        /* Connect our child chopstick nodes */
+        rightChopstick = childNode(withName: "rightChopstick") as! SKSpriteNode
+        leftChopstick = childNode(withName: "leftChopstick") as! SKSpriteNode
+        /* Set the default side */
+        side = .none
     }
     
 }
