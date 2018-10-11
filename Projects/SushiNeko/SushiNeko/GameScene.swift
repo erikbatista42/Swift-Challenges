19b26d211da3ef7b41bbb1460a342de85648771e
ErikBatista <erikbatista42@gmail.com>
2018-10-10 21:31:04 -0700

    morse coade

--- Projects/SushiNeko/SushiNeko/GameScene.swift
+++ Projects/SushiNeko/SushiNeko/GameScene.swift
@@ -18,7 +18,8 @@ class GameScene: SKScene {
     var sushiBasePiece: SushiPiece!
     /* Cat Character */
     var character: Character!
-    
+    /* Sushi tower array */
+    var sushiTower: [SushiPiece] = []
     
     override func didMove(to view: SKView) {
         super.didMove(to: view)
@@ -29,5 +30,37 @@ class GameScene: SKScene {
         /* Connect game objects */
         sushiBasePiece = childNode(withName: "sushiBasePiece") as! SushiPiece
         character = childNode(withName: "character") as! Character
+        /* Manually stack the start of the tower */
+        addTowerPiece(side: .none)
+        addTowerPiece(side: .right)
+    }
+    
+    func addTowerPiece(side: Side) {
+        /* Add a new sushi piece to the sushi tower */
+        
+        /* Copy original sushi piece */
+        let newPiece = sushiBasePiece.copy() as! SushiPiece
+        newPiece.connectChopsticks()
+        
+        /* Access last piece properties */
+        let lastPiece = sushiTower.last
+        
+        /* Add on top of last piece, default on first piece */
+        let lastPosition = lastPiece?.position ?? sushiBasePiece.position
+        newPiece.position.x = lastPosition.x
+        newPiece.position.y = lastPosition.y + 55
+        
+        /* Increment Z to ensure it's on top of the last piece, default on first piece*/
+        let lastZPosition = lastPiece?.zPosition ?? sushiBasePiece.zPosition
+        newPiece.zPosition = lastZPosition + 1
+        
+        /* Set side */
+        newPiece.side = side
+        
+        /* Add sushi to scene */
+        addChild(newPiece)
+        
+        /* Add sushi piece to the sushi tower */
+        sushiTower.append(newPiece)
     }
 }
