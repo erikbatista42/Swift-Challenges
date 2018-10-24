cad6e7fecb9a141e4b15ec611a996445f6b4b8b9
luxury <luxury@luxury.local>
2018-10-24 00:42:56 -0700

    added labels and icon

--- Projects/MagicEightBall/MagicEightBall/ViewController.swift
+++ Projects/MagicEightBall/MagicEightBall/ViewController.swift
@@ -10,11 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var answerLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         print(123)
     }
+    
+    
     
     
     
