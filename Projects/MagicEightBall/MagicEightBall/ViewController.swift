5eede14fe7b1d5e08721d9c922b51b70d0645bdd
luxury <luxury@luxury.local>
2018-10-23 22:39:35 -0700

    added 8 ball

---
+++ Projects/MagicEightBall/MagicEightBall/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  MagicEightBall
+//
+//  Created by luxury on 10/23/18.
+//  Copyright © 2018 luxury. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        print(123)
+    }
+    
+    
+    
+
+
+}
+
