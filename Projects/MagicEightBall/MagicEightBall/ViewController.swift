5826a5199c2db7e8c79f3f01c7376af37241b9f6
luxury <luxury@luxury.local>
2018-10-24 00:57:00 -0700

    finished 8ball tutorial

--- Projects/MagicEightBall/MagicEightBall/ViewController.swift
+++ Projects/MagicEightBall/MagicEightBall/ViewController.swift
@@ -11,11 +11,31 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var answerLabel: UILabel!
+    @IBOutlet weak var shakeButton: UIButton!
+    
+    let answers = ["Lost", "Prison Break", "Full House", "The Office", "F•r•i•e•n•d•s", "I almost got away with it", "Orange is the new black", "Vampire diaries", "Jessica Jones", "Luke Cage", "Iron Fist", "Daredevil"]
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         print(123)
+    }
+    
+    func generateAnswer() {
+        let maxIndex = UInt32(answers.count)
+        let randomIndex = Int(arc4random_uniform(maxIndex))
+        
+        answerLabel.text = answers[randomIndex]
+    }
+    
+    override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        guard motion == .motionShake else { return }
+        generateAnswer()
+    }
+    
+    @IBAction func shakeButtonTapped(_ sender: Any) {
+        generateAnswer()
     }
     
     
