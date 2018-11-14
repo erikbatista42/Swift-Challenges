203390f29fa66db0826da5eef3b66796ef70408d
luxury <erikbatista42@gmail.com>
2018-11-13 21:40:40 -0800

    added segue starter

---
+++ Projects/TableView/TableView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TableView
+//
+//  Created by luxury on 11/8/18.
+//  Copyright © 2018 luxury. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
