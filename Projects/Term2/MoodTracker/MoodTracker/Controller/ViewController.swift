4e4d8621e2c7c73518ba1ba3eb36f1b4cf52363b
luxury <erikbatista42@gmail.com>
2018-11-16 12:57:17 -0800

    fixed table view

---
+++ Projects/Term2/MoodTracker/MoodTracker/Controller/ViewController.swift
@@ -0,0 +1,49 @@
+//
+//  ViewController.swift
+//  MoodTracker
+//
+//  Created by luxury on 11/15/18.
+//  Copyright © 2018 luxury. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    var entries: [MoodEntry] = []
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let goodEntry = MoodEntry(mood: .good, date: Date())
+        let badEntry = MoodEntry(mood: .bad, date: Date())
+        let neutralEntry = MoodEntry(mood: .neutral, date: Date())
+        
+        entries = [goodEntry, badEntry, neutralEntry]
+        tableView.reloadData()
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+
+
+}
+
+extension ViewController: UITableViewDataSource, UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return entries.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "mood entry cell", for: indexPath) as! MoodEntryTableViewCell
+        
+        let entry = entries[indexPath.row]
+        cell.labelMoodTitle.text = entry.mood.stringValue
+        cell.imageViewMoodColor.backgroundColor = entry.mood.colorValue
+        cell.labelMoodDate.text = String(describing: entry.date)
+        
+        return cell
+    }
+}
---
+++ Projects/Term2/MoodTracker/MoodTracker/Model/MoodEntry.swift
@@ -0,0 +1,62 @@
+//
+//  MoodEntry.swift
+//  MoodTracker
+//
+//  Created by luxury on 11/15/18.
+//  Copyright © 2018 luxury. All rights reserved.
+//
+
+import Foundation
+import UIKit.UIColor
+
+struct MoodEntry {
+    var mood: Mood
+    var date: Date
+    
+    enum Mood: Int {
+        case none
+        case amazing
+        case good
+        case neutral
+        case bad
+        case terrible
+        
+        var stringValue: String {
+            switch self {
+            case .none:
+                return ""
+            case .amazing:
+                return "Amazing"
+            case .good:
+                return "Good"
+            case .neutral:
+                return "Neutral"
+            case .bad:
+                return "Bad"
+            case .terrible:
+                return "Terrible"
+            }
+        }
+        
+        var colorValue: UIColor {
+            switch self {
+            case .none:
+                return .clear
+            case .amazing:
+                return .green
+            case .good:
+                return .blue
+            case .neutral:
+                return .gray
+            case .bad:
+                return .orange
+            case .terrible:
+                return .red
+            }
+        }
+    }
+    
+    
+    
+}
+
