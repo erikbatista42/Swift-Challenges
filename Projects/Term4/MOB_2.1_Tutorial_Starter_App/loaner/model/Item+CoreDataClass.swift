8526ee48ac806b7a8a72de0083f7bfe96fdd150f
luxury <erikbatista42@gmail.com>
2019-05-09 14:20:51 -0700

    Created the managed object

---
+++ Projects/Term4/MOB_2.1_Tutorial_Starter_App/loaner/model/Item+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  Item+CoreDataClass.swift
+//  loaner
+//
+//  Created by luxury on 5/9/19.
+//  Copyright © 2019 LinnierGames. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Item)
+public class Item: NSManagedObject {
+
+}
