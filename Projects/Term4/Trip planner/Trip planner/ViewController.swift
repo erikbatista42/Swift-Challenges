dfc5c300d9f62ac392f08f638d3d334fd95fe7ae
luxury <erikbatista42@gmail.com>
2019-05-09 13:44:37 -0700

    added loaner app

---
+++ Projects/Term4/Trip planner/Trip planner/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Trip planner
+//
+//  Created by luxury on 4/29/19.
+//  Copyright © 2019 luxury. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
