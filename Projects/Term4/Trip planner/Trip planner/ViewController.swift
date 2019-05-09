c6e3a3889b25ccd813d10bfd8b5a723d39bdfeb7
luxury <erikbatista42@gmail.com>
2019-05-09 16:28:02 -0700

    screen 1 completed for trip planner

--- Projects/Term4/Trip planner/Trip planner/ViewController.swift
+++ Projects/Term4/Trip planner/Trip planner/ViewController.swift
@@ -8,11 +8,31 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    
+    @IBOutlet weak var tripsTableView: UITableView!
+    
+    @IBAction func addButton(_ sender: Any) {
+        print("add")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        tripsTableView.delegate = self
+        tripsTableView.dataSource = self
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = self.tripsTableView.dequeueReusableCell(withIdentifier: "cellId", for: indexPath)
+        let tripName = "San Jose"
+        cell.textLabel?.text = "Trip to \(tripName)"
+        return cell
     }
 
 
